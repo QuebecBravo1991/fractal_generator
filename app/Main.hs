aa4c37b3984b8e45eff7786b1505a740320cc8f9
Quade Barnes <quade.barnes@gmail.com>
2026-04-10 12:21:35 -0600

    Wrote coordinate to complex number function.

--- app/Main.hs
+++ app/Main.hs
@@ -1,4 +1,9 @@
 module Main (main) where
 
+import Data.Complex
+
+coordToComplex :: Int -> Int -> Complex Int
+coordToComplex x y = x :+ y
+
 main :: IO ()
-main = putStrLn "Hello, Haskell!"
+main = print (coordToComplex 2 3)
