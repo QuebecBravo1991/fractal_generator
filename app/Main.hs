ab52066ed43ec67ad93c01fc345c18ce6c9cf5ef
Quade Barnes <quade.barnes@gmail.com>
2026-04-10 12:51:14 -0600

    Implemented getIters function

--- app/Main.hs
+++ app/Main.hs
@@ -2,8 +2,22 @@ module Main (main) where
 
 import Data.Complex
 
-coordToComplex :: Int -> Int -> Complex Int
-coordToComplex x y = x :+ y
+maxIters :: Int
+maxIters = 1000
+
+coordToComplex :: Int -> Int -> Complex Double
+coordToComplex x y = fromIntegral x :+ fromIntegral y
+
+getIters :: Complex Double -> Int
+getIters c = steps
+  where
+    steps = helper c 0 0
+    helper c' z iters
+      | iters >= maxIters = iters
+      | magnitude newZ > 2 = iters
+      | otherwise = helper c' newZ (iters + 1)
+      where
+        newZ = z * z + c
 
 main :: IO ()
-main = print (coordToComplex 2 3)
+main = print (getIters (coordToComplex 0 0))
